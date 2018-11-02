e04849052388213b5fd11d67b33c58cdca70e53f
Tyler Yasaka <contact@tyleryasaka.me>
2018-11-01 23:33:50 -0700

    bookmark

--- pentaflower/settings.js
+++ pentaflower/settings.js
@@ -1,8 +1,8 @@
 module.exports = {
   colors: {
-    '1': '#759A00',
-    '2': '#FAF2A1',
-    background: '#453F78',
+    '1': '#F3FFE1',
+    '2': '#FFD7D5',
+    background: '#E8D7FF',
   },
   rings: 30
 }
