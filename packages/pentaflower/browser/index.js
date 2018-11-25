c8fbc10ec8b7668e9902852372216b64af82631a
Tyler Yasaka <contact@tyleryasaka.me>
2018-11-25 15:53:30 -0800

    bookmark

--- packages/pentaflower/browser/index.js
+++ packages/pentaflower/browser/index.js
@@ -8,13 +8,17 @@ if (document.body) {
     play = !play
   }
   document.body.onload = function() {
-    pentaflower.setState(0)
+    pentaflower.setState(1)
+    pentaflower.setState(3)
+    pentaflower.setState(25)
+    pentaflower.setState(84)
+    pentaflower.setState(840)
     document.getElementById('canvas').innerHTML = pentaflower.render()
     setInterval(() => {
       if (play) {
         pentaflower.progress()
         document.getElementById('canvas').innerHTML = pentaflower.render()
       }
-    }, 1000)
+    }, 250)
   }
 }
--- packages/pentaflower/settings.js
+++ packages/pentaflower/settings.js
@@ -2,7 +2,7 @@ module.exports = {
   colors: {
     '1': '#429E14',
     '2': '#2A6B0A',
-    background: '#1F271B'
+    background: '#429E14'
   },
-  rings: 70
+  rings: 100
 }
