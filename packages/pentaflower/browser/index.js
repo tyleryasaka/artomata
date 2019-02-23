ed351a919f20958d18e31b1ff283d9ec31c25dea
Tyler Yasaka <contact@tyleryasaka.me>
2019-02-22 18:34:52 -0800

    Create pentaflower-canvas class to simplify browser rendering

--- packages/pentaflower/browser/index.js
+++ packages/pentaflower/browser/index.js
@@ -1,49 +1,12 @@
-const Coord = require('../../../lib/coord')
 const { rings, aliveStates, colors, startCells, interval } = require('../settings')
-const Pentaflower = require('../pentaflower')
+const PentaflowerCanvas = require('../pentaflower-canvas')
 
-const pentaflower = new Pentaflower({ rings, aliveStates })
+const pentaflowerCanvas = new PentaflowerCanvas({ rings, aliveStates, colors, startCells, canvasId: 'canvas' })
 let play = false
 let timer
-let prevFills, fills
-
-const dimensions = pentaflower.getDimensions()
-const fifthX = dimensions.rangeX / 5
-const fifthY = dimensions.rangeY / 5
-const offset = new Coord(dimensions.offset.x, dimensions.offset.y)
-const viewXEnd = dimensions.rangeX - (2 * fifthX)
-const viewYEnd = dimensions.rangeY - (2 * fifthY)
-const points = pentaflower.pentagons.map(p => {
-  return p.points.reduce((acc, point) => {
-    return `${acc} ${point.x + offset.x},${point.y + offset.y}`
-  }, '')
-})
-
-function canvas (fills) {
-  return `\
-    <div id="canvas">
-      <svg xmlns="http://www.w3.org/2000/svg" width="700px" height="700px" viewBox="${fifthX} ${fifthY} ${viewXEnd} ${viewYEnd}" preserveAspectRatio="xMidYMid slice" style="background: ${colors[2]};">
-        ${fills.map((fill, p) => {
-          return `<polygon points="${points[p]}" fill="${fill}" id="poly-${p}" />`
-        })}
-      </svg>
-    </div>
-  `
-}
 
 function update () {
-  pentaflower.progress()
-  fills = pentaflower.pentagons.map(p => p.getState() ? colors[0] : colors[1])
-  const diffs = fills.map((fill, f) => {
-    if (fill !== prevFills[f]) {
-      return { index: f, fill }
-    }
-  })
-  const updates = diffs.filter(d => !!d)
-  updates.forEach(({ index, fill }) => {
-    document.getElementById(`poly-${index}`).setAttribute('fill', fill)
-  })
-  prevFills = fills
+  pentaflowerCanvas.nextT()
 }
 
 if (document.body) {
@@ -60,10 +23,6 @@ if (document.body) {
     }
   }
   document.body.onload = function () {
-    startCells.forEach(s => {
-      pentaflower.setState(s)
-    })
-    prevFills = pentaflower.pentagons.map(p => p.getState() ? colors[0] : colors[1])
-    document.body.innerHTML = canvas(prevFills)
+    pentaflowerCanvas.renderCanvas()
   }
 }
---
+++ packages/pentaflower/pentaflower-canvas.js
@@ -0,0 +1,71 @@
+const Coord = require('../../lib/coord')
+const Pentaflower = require('./pentaflower')
+
+class PentaflowerCanvas {
+  constructor ({ rings, aliveStates, colors, startCells, startT = 0, canvasId }) {
+    this.colors = colors
+    this.pentaflower = new Pentaflower({ rings, aliveStates })
+    startCells.forEach(s => {
+      this.pentaflower.setState(s)
+    })
+    this.prevFills = null
+    this.fills = this.pentaflower.pentagons.map(p => p.getState() ? this.colors[0] : this.colors[1])
+    this.t = 0
+    this.canvasId = canvasId
+    this.hasRenderedCanvas = false
+
+    const dimensions = this.pentaflower.getDimensions()
+    this.fifthX = dimensions.rangeX / 5
+    this.fifthY = dimensions.rangeY / 5
+    const offset = new Coord(dimensions.offset.x, dimensions.offset.y)
+    this.viewXEnd = dimensions.rangeX - (2 * this.fifthX)
+    this.viewYEnd = dimensions.rangeY - (2 * this.fifthY)
+    this.points = this.pentaflower.pentagons.map(p => {
+      return p.points.reduce((acc, point) => {
+        return `${acc} ${point.x + offset.x},${point.y + offset.y}`
+      }, '')
+    })
+    this.prevFills = this.pentaflower.pentagons.map(p => p.getState() ? colors[0] : colors[1])
+    for (let t = 0; t < startT; t++) {
+      this.pentaflower.progress()
+      this.t++
+    }
+  }
+
+  nextT () {
+    this.pentaflower.progress()
+    this.t++
+    this.renderCanvas()
+  }
+
+  renderInitialCanvas () {
+    document.getElementById(this.canvasId).innerHTML = `\
+      <svg xmlns="http://www.w3.org/2000/svg" width="700px" height="700px" viewBox="${this.fifthX} ${this.fifthY} ${this.viewXEnd} ${this.viewYEnd}" preserveAspectRatio="xMidYMid slice" style="background: ${this.colors[2]};">
+        ${this.fills.map((fill, p) => {
+          return `<polygon points="${this.points[p]}" fill="${fill}" id="${this.canvasId}-poly-${p}" />`
+        })}
+      </svg>
+    `
+  }
+
+  renderCanvas () {
+    if (!this.hasRenderedCanvas) {
+      this.renderInitialCanvas()
+      this.hasRenderedCanvas = true
+    } else {
+      this.fills = this.pentaflower.pentagons.map(p => p.getState() ? this.colors[0] : this.colors[1])
+      const diffs = this.fills.map((fill, f) => {
+        if (fill !== this.prevFills[f]) {
+          return { index: f, fill }
+        }
+      })
+      const updates = diffs.filter(d => !!d)
+      updates.forEach(({ index, fill }) => {
+        document.getElementById(`${this.canvasId}-poly-${index}`).setAttribute('fill', fill)
+      })
+      this.prevFills = this.fills
+    }
+  }
+}
+
+module.exports = PentaflowerCanvas
