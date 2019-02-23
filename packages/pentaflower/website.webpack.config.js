0dbe1eb72b110c793e3c67247e8d45e02b08515b
Tyler Yasaka <contact@tyleryasaka.me>
2019-02-23 00:57:42 -0800

    begin constructing artomata website

---
+++ packages/pentaflower/website.webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = [{
+  mode: 'production',
+  entry: './pentaflower-canvas',
+  output: {
+    filename: 'pentaflower-canvas.js',
+    path: path.resolve(__dirname, '../website/dist')
+  }
+}]
---
+++ packages/website/index.js
@@ -0,0 +1,20 @@
+/* globals PentaflowerCanvas */
+
+const pentaflowerCanvas = new PentaflowerCanvas({
+  rings: 50,
+  aliveStates: [1],
+  colors: [
+    '#FFFF00',
+    '#6B6B37',
+    '#99992A'
+  ],
+  startCells: [0],
+  canvasId: 'canvas',
+  startT: 37
+})
+
+if (document.body) {
+  document.body.onload = function () {
+    pentaflowerCanvas.renderCanvas()
+  }
+}
