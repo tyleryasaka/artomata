ff5ba25005dbbeef31b1c21aa88bb89fe9954726
Tyler Yasaka <contact@tyleryasaka.me>
2019-02-23 19:25:14 -0800

    More work on pentaflower exhibit

--- website/_includes/js/pentaflower.js
+++ website/_includes/js/pentaflower.js
@@ -1,6 +1,26 @@
 /* globals PentaflowerCanvas */
 
-const pentaflowerCanvas = new PentaflowerCanvas({
+const canvasConfigs = [{
+  rings: 50,
+  aliveStates: [1],
+  colors: [
+    '#5383D6',
+    '#212C3F',
+    '#4D5E7C'
+  ],
+  startCells: [0],
+  startT: 37
+}, {
+  rings: 50,
+  aliveStates: [1],
+  colors: [
+    '#F79256',
+    '#89441C',
+    '#C94900'
+  ],
+  startCells: [0],
+  startT: 10
+}, {
   rings: 50,
   aliveStates: [1],
   colors: [
@@ -9,12 +29,29 @@ const pentaflowerCanvas = new PentaflowerCanvas({
     '#99992A'
   ],
   startCells: [0],
-  canvasId: 'canvas-1',
-  startT: 37
+  startT: 12
+}, {
+  rings: 50,
+  aliveStates: [1],
+  colors: [
+    '#FFD8DF',
+    '#604D51',
+    '#DB7F90'
+  ],
+  startCells: [0],
+  startT: 87
+}]
+
+const canvases = canvasConfigs.map((config, index) => {
+  config.canvasId = `canvas-${index}`
+  return new PentaflowerCanvas(config)
 })
 
 if (document.body) {
   document.body.onload = function () {
-    pentaflowerCanvas.renderCanvas()
+    canvases.forEach((canvas, index) => {
+      canvas.renderCanvas()
+      document.getElementById(`canvas-${index}`).childNodes[1].style.border = `10px solid ${canvasConfigs[index].colors[1]}`
+    })
   }
 }
