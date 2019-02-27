61690c3d4a46696e2742896b075807baa874f11e
Tyler Yasaka <contact@tyleryasaka.me>
2019-02-27 02:14:52 -0800

    More pentaflowers in exhibit

--- website/_includes/js/pentaflower.js
+++ website/_includes/js/pentaflower.js
@@ -1,17 +1,17 @@
 /* globals PentaflowerCanvas */
 
 const canvasConfigs = [{
-  rings: 50,
+  rings: 48,
   aliveStates: [1],
   colors: [
-    '#5383D6',
-    '#212C3F',
-    '#4D5E7C'
+    '#EF6F6C',
+    '#465775',
+    '#465775'
   ],
   startCells: [0],
-  startT: 37
+  startT: 990
 }, {
-  rings: 50,
+  rings: 41,
   aliveStates: [1],
   colors: [
     '#F79256',
@@ -19,9 +19,9 @@ const canvasConfigs = [{
     '#C94900'
   ],
   startCells: [0],
-  startT: 10
+  startT: 21
 }, {
-  rings: 50,
+  rings: 30,
   aliveStates: [1],
   colors: [
     '#FFFF01',
@@ -31,7 +31,7 @@ const canvasConfigs = [{
   startCells: [0],
   startT: 16
 }, {
-  rings: 50,
+  rings: 46,
   aliveStates: [1],
   colors: [
     '#FFD8DF',
@@ -39,7 +39,107 @@ const canvasConfigs = [{
     '#DB7F90'
   ],
   startCells: [0],
-  startT: 87
+  startT: 288
+}, {
+  rings: 98,
+  aliveStates: [1],
+  colors: [
+    '#ACC6F2',
+    '#212C3F',
+    '#4D5E7C'
+  ],
+  startCells: [0],
+  startT: 210
+}, {
+  rings: 33,
+  aliveStates: [1],
+  colors: [
+    '#72A98F',
+    '#3D5A6C',
+    '#3D5A6C'
+  ],
+  startCells: [0],
+  startT: 995
+}, {
+  rings: 72,
+  aliveStates: [1],
+  colors: [
+    '#D6D5C9',
+    '#902923',
+    '#902923'
+  ],
+  startCells: [0],
+  startT: 391
+}, {
+  rings: 30,
+  aliveStates: [1],
+  colors: [
+    '#FFB627',
+    '#E2711D',
+    '#FFB627'
+  ],
+  startCells: [0],
+  startT: 47
+}, {
+  rings: 40,
+  aliveStates: [1],
+  colors: [
+    '#CEE0DC',
+    '#B48291',
+    '#AFAAB9'
+  ],
+  startCells: [0],
+  startT: 201
+}, {
+  rings: 78,
+  aliveStates: [1],
+  colors: [
+    '#CAFFB9',
+    '#66A182',
+    '#C0D461'
+  ],
+  startCells: [0],
+  startT: 108
+}, {
+  rings: 44,
+  aliveStates: [1],
+  colors: [
+    '#C2F8CB',
+    '#5603AD',
+    '#8367C7'
+  ],
+  startCells: [0],
+  startT: 108
+}, {
+  rings: 100,
+  aliveStates: [1],
+  colors: [
+    '#DA4167',
+    '#FFD166',
+    '#FFD166'
+  ],
+  startCells: [0],
+  startT: 44
+}, {
+  rings: 74,
+  aliveStates: [1],
+  colors: [
+    '#2D728F',
+    '#A0EEE8',
+    '#A0EEE8'
+  ],
+  startCells: [0],
+  startT: 36
+}, {
+  rings: 69,
+  aliveStates: [1],
+  colors: [
+    '#F3CA40',
+    '#577590',
+    '#577590'
+  ],
+  startCells: [0],
+  startT: 31
 }]
 
 const canvases = canvasConfigs.map((config, index) => {
